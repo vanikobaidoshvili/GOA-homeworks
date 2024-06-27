1b0013e405ba946a39ebea108476b95715531f37
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-06-27 18:40:51 +0400

    second commit

---
+++ level 2/lesson 2.py
@@ -0,0 +1,16 @@
+#1)---------------------------------------------
+dad = "giorgi baidoshvili"
+print(dad)
+mum = "ekaterine vardoshvili"
+print(mum)
+sister = "nuca baidoshvili"
+print(sister)
+#3)ჩემი ემოცია GOA-ს შესახებ----------------------
+emotion = "👍😎"
+print(emotion)
+#4)---------------------------------------------
+num1 = 100
+num2 = 200
+num3 = 255
+num4 = 360
+print(num1 + num2 - num3 + num4)
