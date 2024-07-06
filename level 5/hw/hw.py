433c538c4df846b91ff7603c2fe138b05d95c57a
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-07-06 12:17:25 +0400

    second commit

---
+++ level 5/hw/hw.py
@@ -0,0 +1,37 @@
+#1-----------------------------------------------
+num1 = 10
+num2 = 20
+total = num1 + num2
+print(total)
+
+greeting = "Hello"
+age = 25
+message = greeting + ", I am " + str(age) + " years old."
+print(message)
+
+a = 15
+b = 5
+result = a * b
+print("The result of division is:" + str(result))
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+quantity = 3
+item = "apples"
+message = "I have " + str(quantity) + " " + item + "."
+print(message)
+
+greeting = "Hello"
+name = "World"
+full_message = greeting + " " + name
+print(full_message)
+
+a = 50
+b = 100
+result = a + b
+print(result)
+
+zarmaca = "bali 5"
