7471b6290c7db60cd5d99478522a649b35b614c7
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-07-13 20:08:35 +0400

    second commit

---
+++ level 3/cw.py
@@ -0,0 +1,15 @@
+ps5_price = 1500
+print(ps5_price)
+age1=11
+age2=10
+
+print(age1 + age2)
+
+number = "5"
+print(int(number) * 5  )
+#name_surname sance case 
+name = "GOA"
+print(name * 1000)
+
+
+
