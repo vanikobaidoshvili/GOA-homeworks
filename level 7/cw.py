619e4c4d892236de83d6611af9324c653c902a68
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-07-13 19:12:03 +0400

    second commit

---
+++ level 7/cw.py
@@ -0,0 +1,15 @@
+best_food = input("romeli sawmeli mogwons>" )
+print(best_food)
+
+name_and_surname = input("vaniko baiodshvili")
+print(name_and_surname)
+
+numbers = input(55 "and" 23)
+print(55 + 23)
+
+age = input(11)
+print(int(age) + 10)
+
+#input-ს შემოაქვს ინფორმაცია ხოლო output-ს გააქვს ინფორმაცია
+
+#კონკატენაცია არის რამოდენიმე სტრინგის მიმატება
---
+++ level 7/hw.py
@@ -0,0 +1,12 @@
+
+name = input("what is your name ? ")
+surname = input("what is your surname ? ")
+print(name + " " + surname)
+
+
+
+eating_what_I_whant = input("what you want to eat ? I want pizza")
+print(eating_what_I_whant)
+
+
+ 
