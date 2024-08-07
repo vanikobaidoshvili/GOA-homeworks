d06dd89d5039a9e0e8a6d9d471414db8132f72a8
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-08-07 18:36:10 +0400

    second commit

---
+++ python/level 14/hw.py
@@ -0,0 +1,23 @@
+#2)------------------------------------------------------
+agly = False
+beutiful = True
+print(agly or beutiful)
+ok = True
+yes = True
+print(ok or  yes)
+gamer = False
+virgin = False
+print(gamer or virgin)
+bad = False
+good = True
+print(bad and good)
+chad = True
+proggraming = True
+print(chad and proggraming)
+short = False
+tall = False
+print(gamer and virgin)
+#3)-------------------------------------------------------
+is_true = True
+temp = 34 > 32
+print(temp and is_true)
