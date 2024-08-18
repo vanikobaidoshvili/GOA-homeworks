0af322f483dfb7238dabb4256d5f5edebb072ecf
vanikobaidoshvili <vanikobaidoshvili@gmail.com>
2024-08-18 12:16:02 +0400

    second commit

---
+++ day 19/hw.py
@@ -0,0 +1,21 @@
+goa = 1
+while goa < 50:
+    print(goa)
+    goa = goa + 1
+#2)----------------------------------
+acer = 1
+while acer < 50:
+    print(acer)
+    acer = acer + 5
+#3)-----------------------------------
+hp = 50
+while hp > 0:
+    print(hp)
+    hp = hp - 1
+#5)-----------------------------------
+
+password = "SecretWord"
+
+while password != guess:
+    guess = input("enter your password")
+print("access granted")
